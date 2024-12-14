67574e0e02958ca97c81fe5ba9110ddd22e6b064
unknown <pgomez4790@gmail.com>
2024-12-13 21:43:57 -0500

    repair token expire part 2

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,27 +2,29 @@ import { useServices } from "@/hooks/useServices";
 import { AreaChartMultiple, BarChartMultiple, CardAssistsCtoGrande, CardAssistsGlobal, CardAssistsHuanta, CardOfferingGlobal } from "../Private/Dashboard";
 
 export default function Home() {
-  const { data: services } = useServices();
+  const { data: response } = useServices();
 
   return (
     <div className="h-full p-4">
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 p-2">
-                <CardOfferingGlobal />
-                <CardAssistsGlobal />
-
-                <CardAssistsHuanta />
-
-                <CardAssistsCtoGrande />
+      {response && (
+          <>
+          <CardOfferingGlobal data={response.data} />
+          <CardAssistsGlobal data={response.data} />
+          <CardAssistsHuanta data={response.data} />
+          <CardAssistsCtoGrande data={response.data} />
+          </>
+        )}
 
                 
                 
       </div>
 
       <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-2 p-2">
-        {services && (
+      {response && (
           <>
-          <BarChartMultiple data={services} />
-          <AreaChartMultiple data={services} />
+          <BarChartMultiple data={response.data} />
+          <AreaChartMultiple data={response.data}/>
           </>
         )}
         
